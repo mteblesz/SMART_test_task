b2e74e997cc273c8b9384378420c8311074d2b75
mteblesz <mteblesz@gmail.com>
2024-04-08 23:18:04 +0200

    Start project (API + Razor Pages defaults merged)

---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add API Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Razor Pages Services
+builder.Services.AddRazorPages();
+
+// Build
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapRazorPages();
+
+app.Run();
