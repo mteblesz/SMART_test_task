3d64bcb7c84490f176cce4c51f519ded4b26d6c1
mteblesz <mteblesz@gmail.com>
2024-04-15 15:13:41 +0200

    Add layout and nav changes

---
+++ Pages/Coordinator/Coordinator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TMAWarehouse.Pages.Coordinator
+{
+    public class CoordinatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
